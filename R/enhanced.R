66ffca33007a41ac4b9e20e85b7a9909c3d957d4
Adam L. Rich <adamleerich@gmail.com>
2016-08-29 14:39:05 -0400

    Initial commit

---
+++ R/enhanced.R
@@ -0,0 +1,17 @@
+# Copy data out of R
+copy.table <- function(obj, size = 4096) {
+  clip <- paste('clipboard-', size, sep = '')
+  f <- file(description = clip, open = 'w')
+  write.table(obj, f, row.names = FALSE, sep = '\t')
+  close(f)
+}
+
+# Paste data into R
+paste.table <- function() {
+  f <- file(description = 'clipboard', open = 'r')
+  df <- read.table(f, sep = '\t', header = TRUE)
+  close(f)
+  return(df)
+}
+
+
