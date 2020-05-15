8a4861a0e53041c5b059283b00e8b859075836dd
Adam Rich <adamleerich@gmail.com>
2020-05-15 03:48:18 -0400

    Resolved merge conflicts in template stuff

--- data-raw/build-templates.R
+++ data-raw/build-templates.R
@@ -2,8 +2,4 @@ template_file <- './data-raw/test-template.txt'
 test_template <- readChar(template_file, file.info(template_file)$size)
 
 # Save to R/Sysdata.rda
-<<<<<<< HEAD
-devtools::use_data(test_template, internal = TRUE, overwrite = TRUE)
-=======
-devtools::use_data(test_template, internal = TRUE)
->>>>>>> 21ce52281f6ac8841f32e5a7163888ce2023c506
+usethis::use_data(test_template, internal = TRUE, overwrite = TRUE)
