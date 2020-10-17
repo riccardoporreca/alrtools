15d13bb5d1698d80bccd4420d07f9a9969c00532
riccardoporreca <riccardo.porreca@mirai-solutions.com>
2020-10-18 01:39:40 +0200

    Simplify `curry()` with list vector assignment
    * No need to loop nor handle 0-length `...`.
    * This is basically the same approach used to assign `list(null)`.

--- R/all.R
+++ R/all.R
@@ -532,22 +532,11 @@ write.package <- function(pkg, folder) {
 #' function(a) {a + 1}
 #'
 #' @export
-curry <- function(FUN, ...){
-  orig = as.list(match.call())
-  noms = names(orig)
-  if (length(orig) < 3)
-    return(FUN)
-
+curry <- function(FUN, ...) {
+  orig <- utils::tail(as.list(match.call()), -2)
+  noms <- names(orig)
   ff <- formals(FUN)
-  for (i in 3:length(orig)) {
-    # "a value of NULL deletes the corresponding item of the list.
-    # To set entries to NULL, you need x[i] <- list(NULL)."
-    if (is.null(orig[[i]])) {
-      ff[noms[i]] <- list(NULL)
-    } else {
-      ff[[noms[i]]] <- orig[[i]]
-    }
-  }
+  ff[noms] <- orig
   formals(FUN) <- ff
   return(FUN)
 }
